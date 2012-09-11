9cd55b3209f36c2bab3ce49028334993227cbff9
Josh and Kyle <kburton@relaynetwork.com>
2012-09-11 09:13:47 -0400

    lein-release plugin: preparing 1.0.11 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/clorine "1.0.11-SNAPSHOT"
+(defproject com.relaynetwork/clorine "1.0.11"
   :description "Clorine"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
