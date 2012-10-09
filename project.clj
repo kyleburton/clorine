bde47a22359180a485aafda5a8341242b73f5cdd
Kyle Burton <kburton@relaynetwork.com>
2012-10-08 23:14:56 -0400

    lein-release plugin: bumped version from 1.3.12 to 1.3.13-SNAPSHOT for next developemnt cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/clorine "1.3.12"
+(defproject com.relaynetwork/clorine "1.3.13-SNAPSHOT"
   :description "Clorine"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
