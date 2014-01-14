611f26520e0e2bc5e2999ed651719be6f678f699
Paul and Kyle <kburton@relaynetwork.com>
2014-01-14 18:12:52 -0500

    lein-release plugin: preparing 1.0.17 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/clorine "1.0.17-SNAPSHOT"
+(defproject com.relaynetwork/clorine "1.0.17"
   :description  "Clorine"
   :url          "http://github.com/relaynetwork/clorine"
   :lein-release {:deploy-via :clojars}
